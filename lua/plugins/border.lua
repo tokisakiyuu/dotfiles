7fa9992f3b47f72e3b651f419f4b776ce8ab1474
tokisakiyuu <tokisakiyuu@qq.com>
2024-07-30 10:12:14 +0800

    improve config struct and add popups border
    https://github.com/LazyVim/LazyVim/discussions/4220#discussioncomment-10181733

---
+++ lua/plugins/border.lua
@@ -0,0 +1,41 @@
+vim.opt.pumblend = 0
+vim.opt.winblend = 0
+
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      }
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ui = {
+        border = "rounded",
+      },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    opts = {
+      presets = {
+        lsp_doc_border = true,
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      diagnostics = {
+        float = {
+          border = "rounded",
+        },
+      },
+    },
+  },
+}
---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,103 @@
+-- keep it 👆 in main theme
+
+return {
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      { "<leader>b[", "<cmd>BufferLineMovePrev<CR>", desc = "Exchange with Prev Buffer" },
+      { "<leader>b]", "<cmd>BufferLineMoveNext<CR>", desc = "Exchange with Next Buffer" },
+      { "gb", "<cmd>BufferLinePick<CR>", desc = "Pick Buffer" },
+    },
+    opts = {
+      options = {
+        show_buffer_close_icons = false,
+        -- always_show_bufferline = true,
+        separator_style = { "", "" },
+        indicator = { style = "none" },
+      },
+      highlights = {
+        tab_selected = {
+          bg = "#001595",
+        },
+        tab_separator_selected = {
+          bg = "#001595",
+        },
+        close_button_selected = {
+          bg = "#001595",
+        },
+        buffer_selected = {
+          bg = "#001595",
+          bold = true,
+          italic = false,
+        },
+        numbers_selected = {
+          bg = "#001595",
+          bold = true,
+          italic = false,
+        },
+        diagnostic_selected = {
+          bg = "#001595",
+          bold = true,
+          italic = false,
+        },
+        hint_selected = {
+          bg = "#001595",
+          bold = true,
+          italic = false,
+        },
+        hint_diagnostic_selected = {
+          bg = "#001595",
+        },
+        info_selected = {
+          bg = "#001595",
+          bold = true,
+          italic = false,
+        },
+        info_diagnostic_selected = {
+          bg = "#001595",
+        },
+        warning_selected = {
+          bg = "#001595",
+          bold = true,
+          italic = false,
+        },
+        warning_diagnostic_selected = {
+          bg = "#001595",
+        },
+        error_selected = {
+          bg = "#001595",
+          bold = true,
+          italic = false,
+        },
+        error_diagnostic_selected = {
+          bg = "#001595",
+        },
+        modified_selected = {
+          bg = "#001595",
+          bold = true,
+          italic = false,
+        },
+        duplicate_selected = {
+          bg = "#001595",
+          bold = true,
+          italic = false,
+        },
+        separator_selected = {
+          bg = "#001595",
+          bold = true,
+          italic = false,
+        },
+        indicator_selected = {
+          bg = "#001595",
+          bold = true,
+          italic = false,
+        },
+        pick_selected = {
+          bg = "#001595",
+          bold = true,
+          italic = false,
+        },
+      },
+    },
+  },
+}

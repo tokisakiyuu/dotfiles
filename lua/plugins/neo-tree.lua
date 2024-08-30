2a125104ad5ce9f8af00709779dcd653d77475c7
tokisakiyuu <tokisakiyuu@qq.com>
2024-08-30 12:40:52 +0800

    custom configuration for neo tree

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      {
+        "<leader>bb",
+        "<cmd>Neotree source=buffers reveal=true position=bottom<CR> ",
+        desc = "NeoTree Buffers",
+      },
+    },
+    opts = {
+      window = {
+        mappings = {
+          ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+        },
+      },
+      buffers = {
+        show_unloaded = true,
+      },
+    },
+  },
+}

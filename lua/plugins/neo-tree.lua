df4f87396f3df88fc4c12a880dc78aaf5bfe3b98
tokisakiyuu <tokisakiyuu@qq.com>
2024-11-12 22:51:42 +0800

    update neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,12 +10,25 @@ return {
     },
     opts = {
       window = {
+        position = "right",
         mappings = {
           ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
         },
       },
       buffers = {
         show_unloaded = true,
+      },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_by_name = {
+            ".git",
+            ".DS_Store",
+          },
+          always_show = {
+            ".env",
+          },
+        },
       },
       default_component_configs = {
         container = {

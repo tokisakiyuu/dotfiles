f54602e60a5874da0d4ce02622911cb86ab54121
tokisakiyuu <tokisakiyuu@qq.com>
2024-11-18 14:16:04 +0800

    enable bigfile editing support

--- lua/plugins/tokisakiyuu.lua
+++ lua/plugins/tokisakiyuu.lua
@@ -29,8 +29,10 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
-    ---@type snacks.Config
     opts = {
+      bigfile = {
+        enabled = true,
+      },
       styles = {
         lazygit = {
           border = "rounded",

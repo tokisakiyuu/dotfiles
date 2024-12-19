9714cf3557550128fecba70152963c581628a8b9
tokisakiyuu <tokisakiyuu@qq.com>
2024-12-19 23:35:31 +0800

    nvim: restore bufferline style

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,4 +1,4 @@
--- keep it 👆 in main theme
+-- 👆 This bar
 
 return {
   {
@@ -16,89 +16,89 @@ return {
         persist_buffer_sort = false,
         sort_by = "insert_after_current",
       },
-      highlights = {
-        tab_selected = {
-          bg = "#001595",
-        },
-        tab_separator_selected = {
-          bg = "#001595",
-        },
-        close_button_selected = {
-          bg = "#001595",
-        },
-        buffer_selected = {
-          bg = "#001595",
-          bold = true,
-          italic = false,
-        },
-        numbers_selected = {
-          bg = "#001595",
-          bold = true,
-          italic = false,
-        },
-        diagnostic_selected = {
-          bg = "#001595",
-          bold = true,
-          italic = false,
-        },
-        hint_selected = {
-          bg = "#001595",
-          bold = true,
-          italic = false,
-        },
-        hint_diagnostic_selected = {
-          bg = "#001595",
-        },
-        info_selected = {
-          bg = "#001595",
-          bold = true,
-          italic = false,
-        },
-        info_diagnostic_selected = {
-          bg = "#001595",
-        },
-        warning_selected = {
-          bg = "#001595",
-          bold = true,
-          italic = false,
-        },
-        warning_diagnostic_selected = {
-          bg = "#001595",
-        },
-        error_selected = {
-          bg = "#001595",
-          bold = true,
-          italic = false,
-        },
-        error_diagnostic_selected = {
-          bg = "#001595",
-        },
-        modified_selected = {
-          bg = "#001595",
-          bold = true,
-          italic = false,
-        },
-        duplicate_selected = {
-          bg = "#001595",
-          bold = true,
-          italic = false,
-        },
-        separator_selected = {
-          bg = "#001595",
-          bold = true,
-          italic = false,
-        },
-        indicator_selected = {
-          bg = "#001595",
-          bold = true,
-          italic = false,
-        },
-        pick_selected = {
-          bg = "#001595",
-          bold = true,
-          italic = false,
-        },
-      },
+      -- highlights = {
+      --   tab_selected = {
+      --     bg = "#001595",
+      --   },
+      --   tab_separator_selected = {
+      --     bg = "#001595",
+      --   },
+      --   close_button_selected = {
+      --     bg = "#001595",
+      --   },
+      --   buffer_selected = {
+      --     bg = "#001595",
+      --     bold = true,
+      --     italic = false,
+      --   },
+      --   numbers_selected = {
+      --     bg = "#001595",
+      --     bold = true,
+      --     italic = false,
+      --   },
+      --   diagnostic_selected = {
+      --     bg = "#001595",
+      --     bold = true,
+      --     italic = false,
+      --   },
+      --   hint_selected = {
+      --     bg = "#001595",
+      --     bold = true,
+      --     italic = false,
+      --   },
+      --   hint_diagnostic_selected = {
+      --     bg = "#001595",
+      --   },
+      --   info_selected = {
+      --     bg = "#001595",
+      --     bold = true,
+      --     italic = false,
+      --   },
+      --   info_diagnostic_selected = {
+      --     bg = "#001595",
+      --   },
+      --   warning_selected = {
+      --     bg = "#001595",
+      --     bold = true,
+      --     italic = false,
+      --   },
+      --   warning_diagnostic_selected = {
+      --     bg = "#001595",
+      --   },
+      --   error_selected = {
+      --     bg = "#001595",
+      --     bold = true,
+      --     italic = false,
+      --   },
+      --   error_diagnostic_selected = {
+      --     bg = "#001595",
+      --   },
+      --   modified_selected = {
+      --     bg = "#001595",
+      --     bold = true,
+      --     italic = false,
+      --   },
+      --   duplicate_selected = {
+      --     bg = "#001595",
+      --     bold = true,
+      --     italic = false,
+      --   },
+      --   separator_selected = {
+      --     bg = "#001595",
+      --     bold = true,
+      --     italic = false,
+      --   },
+      --   indicator_selected = {
+      --     bg = "#001595",
+      --     bold = true,
+      --     italic = false,
+      --   },
+      --   pick_selected = {
+      --     bg = "#001595",
+      --     bold = true,
+      --     italic = false,
+      --   },
+      -- },
     },
   },
 }

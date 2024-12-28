da2b23146062be5ec6f6da29af67648fa6d6c6c6
tokisakiyuu <tokisakiyuu@qq.com>
2024-12-28 18:49:25 +0800

    nvim: selector config

---
+++ nvim/.config/nvim/lua/plugins/selector.lua
@@ -0,0 +1,14 @@
+-- http://www.lazyvim.org/extras/editor/fzf#fzf-lua
+return {
+  {
+    "ibhagwan/fzf-lua",
+    opts = {
+      winopts = {
+        fullscreen = true,
+        preview = {
+          layout = "vertical",
+        },
+      },
+    },
+  },
+}

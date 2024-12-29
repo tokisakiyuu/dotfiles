4fc9c01802b907027412736e04afaaca37fbfb6c
tokisakiyuu <tokisakiyuu@qq.com>
2024-12-29 18:58:25 +0800

    nvim: changes to selector configurations

--- nvim/.config/nvim/lua/plugins/selector.lua
+++ nvim/.config/nvim/lua/plugins/selector.lua
@@ -1,13 +1,22 @@
+local fullscreen_win_opts = {
+  fullscreen = true,
+  preview = {
+    layout = "vertical",
+    vertical = "down:60%",
+  },
+}
+
 -- http://www.lazyvim.org/extras/editor/fzf#fzf-lua
+-- https://github.com/ibhagwan/fzf-lua/tree/main?tab=readme-ov-file#customization
 return {
   {
     "ibhagwan/fzf-lua",
     opts = {
-      winopts = {
-        fullscreen = true,
-        preview = {
-          layout = "vertical",
-        },
+      files = {
+        winopts = fullscreen_win_opts,
+      },
+      grep = {
+        winopts = fullscreen_win_opts,
       },
     },
   },

be3e0b57477a6908bd4f82f84f66cf7a2c76ac58
tokisakiyuu <tokisakiyuu@M4.local>
2025-01-09 00:13:56 +0800

    fish: fix fish bin not found

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Homebrew
+/opt/homebrew/bin/brew shellenv | source
+
 # Terminal shell prompt (installed by Homebrew)
 # https://starship.rs/
 starship init fish | source
@@ -15,23 +18,26 @@ if test -n (lsof -Pi :7890 -sTCP:LISTEN | string collect)
     set -gx all_proxy socks5://127.0.0.1:7890
 end
 
-set -gx SHELL /usr/local/bin/fish
+set -gx SHELL /opt/homebrew/bin/fish
+
+# XDG basedir
+# https://wiki.archlinux.org/title/XDG_Base_Directory
 
 # Suppress fish default greeting
 set -g fish_greeting ''
 
 # pnpm home
-set -gx PNPM_HOME "$HOME/Library/pnpm"
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME:$PATH"
 
 # Cargo
-set -gx PATH "$HOME/.cargo/bin:$PATH"
+set -gx PATH "$HOME/.local/share/cargo/bin:$PATH"
 
 # Terminal programs UI language
 set -gx LANG "en_US.UTF-8"
 
 # avante.nvim API Key
-set -gx OPENAI_API_KEY (keychain-env KIMI_API_KEY)
+# set -gx OPENAI_API_KEY (keychain-env KIMI_API_KEY)
 
 # zoxide, z command (installed by Homebrew)
 set -gx _ZO_DATA_DIR "$HOME/.local/share/z"

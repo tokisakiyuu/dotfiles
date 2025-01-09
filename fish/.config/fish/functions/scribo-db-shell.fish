632b54e894c0c3af86d4f459d73133a15ee39ff3
tokisakiyuu <tokisakiyuu@M4.local>
2025-01-09 23:48:55 +0800

    fish: change to file path

--- fish/.config/fish/functions/scribo-db-shell.fish
+++ fish/.config/fish/functions/scribo-db-shell.fish
@@ -1,4 +1,4 @@
 function scribo-db-shell --wraps=mongosh
     set -lx SCRIBO_MONGO_SHELL_ENV $argv[1]
-    mongosh --file ~/space/mywork/scribo-mongosh.js --nodb --shell
+    mongosh --file ~/space/work/scribo-mongosh.js --nodb --shell
 end

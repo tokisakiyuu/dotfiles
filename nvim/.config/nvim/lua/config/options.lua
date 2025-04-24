0038694c7e36f8c5567939bd8d62a5b149bb9b21
tokisakiyuu <tokisakiyuu@qq.com>
2025-04-24 10:49:43 +0800

    nvim: set terminal window title

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,9 +2,17 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.snacks_animate = false
-vim.g.root_spec = { "cwd" }
-
 local opt = vim.opt
 
+-- Dsiable snacks.nvim scrolling animation
+vim.g.snacks_animate = false
+
+-- Always take cwd as working directory
+vim.g.root_spec = { "cwd" }
+
+-- Ctrl+i/o behavior
 opt.jumpoptions = "stack"
+
+-- Set terminal window title
+vim.o.title = true
+vim.o.titlestring = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")

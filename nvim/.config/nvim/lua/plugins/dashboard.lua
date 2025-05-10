1f5d77d686847980f74ce5a8721a6347423f7a49
tokisakiyuu <tokisakiyuu@qq.com>
2025-05-10 22:59:29 +0800

    nvim: modify dashboard config

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -3,7 +3,41 @@ return {
     "snacks.nvim",
     opts = {
       dashboard = {
+        -- https://github.com/folke/snacks.nvim/blob/main/docs/dashboard.md#advanced
+        sections = {
+          { section = "header" },
+          {
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+          { section = "keys", gap = 1, padding = 1 },
+          { section = "startup" },
+        },
+
         preset = {
+          -- https://github.com/folke/snacks.nvim/blob/main/docs/dashboard.md#%EF%B8%8F-config
+          keys = {
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+            {
+              icon = "󰒲 ",
+              key = "e",
+              desc = "Lazy Extras",
+              action = ":LazyExtras",
+              enabled = package.loaded.lazy ~= nil,
+            },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+
           -- https://github.com/MaximilianLloyd/ascii.nvim
           header = table.concat({
             [[                                                                       ]],

1cfa4c19222f2bbdf8890dea2952def265551d57
tokisakiyuu <tokisakiyuu@qq.com>
2025-05-12 16:15:00 +0800

    nvim: modify dashboard config

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -6,23 +6,20 @@ return {
         -- https://github.com/folke/snacks.nvim/blob/main/docs/dashboard.md#advanced
         sections = {
           { section = "header" },
-          {
-            {
-              icon = " ",
-              title = "Git Status",
-              section = "terminal",
-              enabled = function()
-                return Snacks.git.get_root() ~= nil
-              end,
-              cmd = "git status --short --branch --renames",
-              height = 5,
-              padding = 1,
-              ttl = 0,
-              indent = 3,
-            },
-          },
           { section = "keys", gap = 1, padding = 1 },
-          { section = "startup" },
+          {
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = function()
+              return Snacks.git.get_root() ~= nil
+            end,
+            cmd = "git status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 0,
+            indent = 3,
+          },
         },
 
         preset = {
@@ -37,6 +34,16 @@ return {
               action = ":LazyExtras",
               enabled = package.loaded.lazy ~= nil,
             },
+            {
+              icon = " ",
+              desc = "Browse Repo",
+              padding = 1,
+              key = "b",
+              action = function()
+                Snacks.gitbrowse()
+              end,
+            },
+
             { icon = " ", key = "q", desc = "Quit", action = ":qa" },
           },
 

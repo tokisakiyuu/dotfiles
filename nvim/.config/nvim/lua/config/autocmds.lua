92ec49235bd4cd4efb457ca06f20ffdf8ca61647
tokisakiyuu <tokisakiyuu@qq.com>
2025-07-01 17:22:11 +0800

    nvim: improve previous keymap function

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.js", "*.ts", "*.jsx", "*.tsx" },
+  callback = function()
+    vim.keymap.set(
+      "n",
+      "<leader>cp",
+      "yiwoconsole.log('<Esc>pa', <Esc>pa)<Esc>",
+      { desc = "Print javascript variable", buffer = true }
+    )
+  end,
+})

d400bf6251ae47041a0d8596c491a7990f3afbff
tokisakiyuu <tokisakiyuu@qq.com>
2025-07-01 17:04:34 +0800

    nvim: add keymap for quick adding console.log statement

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("v", "<leader>cp", "yoconsole.log('<Esc>pa', <Esc>pa)<Esc>", { desc = "Print javascript variable" })

7d1704ea4e29581e6e91cc963b42c7cc308cbeb9
tokisakiyuu <tokisakiyuu@qq.com>
2025-07-06 16:33:30 +0800

    nvim: setup tailwindcss lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -17,13 +17,63 @@ return {
     opts = {
       inlay_hints = { enabled = false },
       codelens = { enabled = false },
+      servers = {
+        -- Custom Server Options
+        -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+      },
     },
   },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
+        "typescript",
+        "javascript",
+        "json",
+        "rust",
+        "tsx",
         "graphql",
+        "prisma",
+        "toml",
+        "html",
+        "jsdoc",
+        "lua",
+      },
+    },
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "vtsls", -- typescript lsp
+        "tailwindcss",
+        "rust_analyzer", -- rsut lsp
+        "eslint",
+        "graphql",
+        "jsonls", -- json files lsp
+      },
+    },
+  },
+
+  -- Tailwind CSS Tools (Hover Documentaion, LSP Configuration)
+  {
+    "luckasRanarison/tailwind-tools.nvim",
+    name = "tailwind-tools",
+    build = ":UpdateRemotePlugins",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "neovim/nvim-lspconfig", -- optional
+    },
+    opts = {
+      server = {
+        -- https://github.com/tailwindlabs/tailwindcss-intellisense?tab=readme-ov-file#extension-settings
+        settings = {
+          classFunctions = {
+            "tw",
+            "clsx",
+          },
+          colorDecorators = false,
+        },
       },
     },
   },

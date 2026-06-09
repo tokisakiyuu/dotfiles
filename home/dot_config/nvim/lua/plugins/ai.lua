1bb3115bbb869d53a22c11c5b80ed41435ac0315
tokisakiyuu <tokisakiyuu@qq.com>
2026-06-09 21:16:08 +0800

    update claudecode.nvim config

--- home/dot_config/nvim/lua/plugins/ai.lua
+++ home/dot_config/nvim/lua/plugins/ai.lua
@@ -5,6 +5,16 @@ return {
     keys = {
       { "<leader>a", nil, desc = "AI/Claude Code" },
       { "<leader>ab", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+      { "<leader>as", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send to Claude" },
+      {
+        "<leader>as",
+        "<cmd>ClaudeCodeTreeAdd<cr>",
+        desc = "Add file",
+        ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw", "snacks_picker_list" },
+      },
+      -- Diff management
+      { "<leader>aa", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+      { "<leader>ad", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
     },
     config = true,
     opts = {
